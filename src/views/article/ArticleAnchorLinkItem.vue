ba5d60166b710b2078f9ad0cb5a91a3448950ca5
hhoao <haunghao@foxmail.com>
2025-04-11 12:10:31 +0800

    feat: fix delete build problem

---
+++ src/views/article/ArticleAnchorLinkItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <anchor-link :key="item.name" v-for="item in items" :href="item.path" :title="item.name">
+    <article-anchor-link-item
+      v-if="item.children && item.children.length != 0"
+      :items="item.children"
+    />
+  </anchor-link>
+</template>
+
+<script setup lang="ts">
+  import { AnchorLink } from '/@/utils/outline';
+
+  defineProps({
+    items: { type: Object as PropType<AnchorLink[]> },
+  });
+</script>
+
+<style scoped></style>
